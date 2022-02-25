d4ee31c06a301ec70da20a2dca4ce8b65111238c
Nandha <nandhamass16@gmail.com>
2022-02-25 18:48:52 +0530

    Add project files.

---
+++ Models/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MyWepAPI2.Models
+{
+    public partial class Employee
+    {
+        [Key]
+        [Column("EmployeeID")]
+        public int? EmployeeId { get; set; }
+        [StringLength(50)]
+        public string? Name { get; set; }
+        [StringLength(50)]
+        public string? Position { get; set; }
+        public int? Age { get; set; }
+        public int? Salary { get; set; }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using MyWepAPI2.Models;
+
+namespace MyWepAPI2
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.      
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+            services.AddDbContext<CoreDbContext>(op => op.UseSqlServer(Configuration.GetConnectionString("Server=(LocalDB)\\MSSQLLocalDB;Database=CRUDDB;Trusted_Connection=True;"))); //Add       
+            services.AddControllers();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "MyWebAPI2",
+                    Version = "v1",
+                });
+
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.      
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c => { c.SwaggerEndpoint("/swagger/v1/swagger.json", "MyWebAPI2 V1"); });
+            app.UseHttpsRedirection();
+            app.UseRouting();
+            app.UseAuthorization();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
